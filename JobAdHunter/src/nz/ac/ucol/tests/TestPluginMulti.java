b5a493946e8810d8b140d86f920df4bb292f9081
Tim McGrath <timothy_mcgrath67@hotmail.com>
2015-05-27 21:16:38 +1200

    plugin source for testing multi pages

---
+++ JobAdHunter/src/nz/ac/ucol/tests/TestPluginMulti.java
@@ -0,0 +1,56 @@
+package nz.ac.ucol.tests;
+
+import java.util.ArrayList;
+
+import nz.ac.ucol.configuration.JobAd;
+import nz.ac.ucol.plugin.SiteHandler;
+
+public class TestPluginMulti implements SiteHandler {
+
+	@Override
+	public ArrayList<JobAd> processSiteSearchResults(String site) {
+		ArrayList<JobAd> jobs = new ArrayList<>();
+		
+		site = site.split("(?i)<ul>")[1];
+		site = site.split("(?i)</ul>")[0];
+		String[] sections = site.split("(?i)<li>");
+		for(int i = 1; i < sections.length;i++)
+		{
+			String section = sections[i];
+			section = section.replace("(?i)</li>", "");
+			String[] jobparts = section.split("(?i)<br>");
+			
+			JobAd job = new JobAd();
+			job.setTitle(jobparts[0]);
+			job.setUrlOFAditionalInfo(jobparts[1]);
+			job.setShortDescription(jobparts[2]);
+			job.setSource(site);
+			jobs.add(job);
+		}
+		return jobs;
+	}
+
+	@Override
+	public JobAd processSiteJobListing(String site, JobAd jobAdToAddInfoToo) {
+		String[] result = site.split("(?i)<br />");
+		JobAd jobAd = new JobAd();
+		String fullDescription = "";
+		for(int i = 1; i < result.length;i++)
+		{
+			fullDescription += result[i];
+		}
+		jobAd.setFullDescription(fullDescription);
+		return jobAd;
+	}
+
+	@Override
+	public String handlesSite() {
+		return "testSeperatePages";
+	}
+
+	@Override
+	public String getURLofNextPage() {
+		return "testSeperatePagesTwo.html";
+	}
+
+}
